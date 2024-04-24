5a503c954bfdc3fdfbac2ebde7eec5569088b412
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2024-04-24 19:29:33 +0200

    Initialize project

---
+++ src/main/java/io/github/wkktoria/managetime/ManagetimeApplication.java
@@ -0,0 +1,13 @@
+package io.github.wkktoria.managetime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagetimeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManagetimeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/wkktoria/managetime/ManagetimeApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.wkktoria.managetime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagetimeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
