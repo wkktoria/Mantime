7f453794dd744ca7e6f123c562f7cb7fb3a19da5
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2024-04-25 21:44:14 +0200

    Add method to find by done to task repository

--- src/main/java/io/github/wkktoria/managetime/model/TaskRepository.java
+++ src/main/java/io/github/wkktoria/managetime/model/TaskRepository.java
@@ -1,8 +1,14 @@
 package io.github.wkktoria.managetime.model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+import java.util.List;
 
 @RepositoryRestResource
 interface TaskRepository extends JpaRepository<Task, Long> {
+    @RestResource(path = "done", rel = "done")
+    List<Task> findByDone(@Param("status") boolean done);
 }
