4821bcbe10393fb4103dcc749c02f6445552754b
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2024-04-26 18:08:42 +0200

    Remove validation configuration from main

--- src/main/java/io/github/wkktoria/managetime/ManagetimeApplication.java
+++ src/main/java/io/github/wkktoria/managetime/ManagetimeApplication.java
@@ -3,14 +3,12 @@ package io.github.wkktoria.managetime;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.rest.core.event.ValidatingRepositoryEventListener;
-import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.validation.Validator;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 
 @SpringBootApplication
-public class ManagetimeApplication implements RepositoryRestConfigurer {
+public class ManagetimeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ManagetimeApplication.class, args);
@@ -19,11 +17,5 @@ public class ManagetimeApplication implements RepositoryRestConfigurer {
     @Bean
     Validator validator() {
         return new LocalValidatorFactoryBean();
-    }
-
-    @Override
-    public void configureValidatingRepositoryEventListener(final ValidatingRepositoryEventListener validatingListener) {
-        validatingListener.addValidator("beforeCreate", validator());
-        validatingListener.addValidator("beforeSave", validator());
     }
 }
