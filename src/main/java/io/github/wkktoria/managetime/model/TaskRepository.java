6324637e9f714fc87fb1aa3abd8ff22c9af3ce20
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2024-04-26 11:48:36 +0200

    Use @RestController

--- src/main/java/io/github/wkktoria/managetime/model/TaskRepository.java
+++ src/main/java/io/github/wkktoria/managetime/model/TaskRepository.java
@@ -2,12 +2,12 @@ package io.github.wkktoria.managetime.model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@RepositoryRestResource
+@Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
     @RestResource(path = "done", rel = "done")
     List<Task> findByDone(@Param("status") boolean done);
