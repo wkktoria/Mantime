c6b348d24ce9f6fbfe7514520aff0ff9754824a2
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2024-05-09 17:29:32 +0200

    Update SqlTaskRepository
    Suppress warnings and override a method

--- src/main/java/io/github/wkktoria/managetime/model/SqlTaskRepository.java
+++ src/main/java/io/github/wkktoria/managetime/model/SqlTaskRepository.java
@@ -1,9 +1,15 @@
 package io.github.wkktoria.managetime.model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
 
+@SuppressWarnings("unused")
 @Repository
 interface SqlTaskRepository extends JpaRepository<Task, Long>, TaskRepository {
-
+    @Override
+    @Query(nativeQuery = true, value = "select count(*) > 0 from tasks where id=:id")
+    boolean existsById(@Param("id") @NonNull Long id);
 }
