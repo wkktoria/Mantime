04d2bbec49684a377422f2e910ec24d8e2204c7e
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2024-05-22 21:48:51 +0200

    Create task group entity

---
+++ src/main/java/io/github/wkktoria/managetime/model/TaskGroup.java
@@ -0,0 +1,44 @@
+package io.github.wkktoria.managetime.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "task_group")
+public class TaskGroup {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotBlank(message = "Task group description cannot be empty")
+    private String description;
+    private boolean done;
+    @Embedded
+    private Audit audit = new Audit();
+
+    public TaskGroup() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    void setId(final Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    void setDescription(final String description) {
+        this.description = description;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    void setDone(final boolean done) {
+        this.done = done;
+    }
+}
