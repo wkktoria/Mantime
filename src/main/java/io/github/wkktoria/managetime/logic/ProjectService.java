2570e0b2e3c827326cd166f410be9931a8c26de0
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2024-05-23 14:22:08 +0200

    Add Project service

---
+++ src/main/java/io/github/wkktoria/managetime/logic/ProjectService.java
@@ -0,0 +1,47 @@
+package io.github.wkktoria.managetime.logic;
+
+import io.github.wkktoria.managetime.TaskConfigurationProperties;
+import io.github.wkktoria.managetime.model.*;
+import io.github.wkktoria.managetime.model.projection.GroupReadModel;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class ProjectService {
+    private ProjectRepository repository;
+    private TaskGroupRepository taskGroupRepository;
+    private TaskConfigurationProperties config;
+
+    public ProjectService(final ProjectRepository repository, final TaskGroupRepository taskGroupRepository, final TaskConfigurationProperties config) {
+        this.repository = repository;
+        this.taskGroupRepository = taskGroupRepository;
+        this.config = config;
+    }
+
+    public List<Project> readAll() {
+        return repository.findAll();
+    }
+
+    public Project save(Project toSave) {
+        return repository.save(toSave);
+    }
+
+    public GroupReadModel createGroup(LocalDateTime deadline, Long projectId) {
+        if (config.getTemplate().isAllowMultipleTasks() && taskGroupRepository.existsByDoneIsFalseAndProjectId(projectId)) {
+            throw new IllegalStateException("Only one undone group from project is allowed");
+        }
+        TaskGroup result = repository.findById(projectId)
+                .map(project -> {
+                    TaskGroup targetGroup = new TaskGroup();
+                    targetGroup.setDescription(project.getDescription());
+                    targetGroup.setTasks(project.getSteps().stream()
+                            .map(step -> new Task(step.getDescription(), deadline.plusDays(step.getDaysToDeadline())))
+                            .collect(Collectors.toSet()));
+                    return targetGroup;
+                }).orElseThrow(() -> new IllegalArgumentException("Project with given id not found"));
+        return new GroupReadModel(result);
+    }
+}
