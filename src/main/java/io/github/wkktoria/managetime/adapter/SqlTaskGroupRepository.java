7a3c241cd3aeda3b9740239551df7c561aeacb7e
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2024-05-23 10:32:01 +0200

    Add method to SqlTaskGroupRepository interface

--- src/main/java/io/github/wkktoria/managetime/adapter/SqlTaskGroupRepository.java
+++ src/main/java/io/github/wkktoria/managetime/adapter/SqlTaskGroupRepository.java
@@ -2,8 +2,14 @@ package io.github.wkktoria.managetime.adapter;
 
 import io.github.wkktoria.managetime.model.TaskGroup;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 interface SqlTaskGroupRepository extends JpaRepository<TaskGroup, Long> {
+    @Override
+    @Query("select distinct  tg from TaskGroup tg join fetch tg.tasks")
+    List<TaskGroup> findAll();
 }
