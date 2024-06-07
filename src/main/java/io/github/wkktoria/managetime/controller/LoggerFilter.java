81cce56d3eb06ac72cabd419fa8aa3e864650de9
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2024-06-07 11:03:00 +0200

    Create logger filter

---
+++ src/main/java/io/github/wkktoria/managetime/controller/LoggerFilter.java
@@ -0,0 +1,26 @@
+package io.github.wkktoria.managetime.controller;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+class LoggerFilter implements Filter {
+    private static final Logger logger = LoggerFactory.getLogger(LoggerFilter.class);
+
+    @Override
+    public void doFilter(final ServletRequest servletRequest, final ServletResponse servletResponse, final FilterChain filterChain) throws IOException, ServletException {
+        if (servletRequest instanceof HttpServletRequest) {
+            HttpServletRequest request = (HttpServletRequest) servletRequest;
+            logger.info("Request URI: {}; Request method: {}", request.getRequestURI(), request.getMethod());
+        }
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+}
