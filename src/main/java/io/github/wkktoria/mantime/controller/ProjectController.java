4854373a5497b3aa0e7dcc44118094d8474d1b6f
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2024-06-23 15:36:23 +0200

    Create groups from projects

--- src/main/java/io/github/wkktoria/mantime/controller/ProjectController.java
+++ src/main/java/io/github/wkktoria/mantime/controller/ProjectController.java
@@ -5,14 +5,13 @@ import io.github.wkktoria.mantime.model.Project;
 import io.github.wkktoria.mantime.model.ProjectStep;
 import io.github.wkktoria.mantime.model.projection.ProjectWriteModel;
 import jakarta.validation.Valid;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Controller
@@ -39,13 +38,27 @@ class ProjectController {
         }
         service.save(project);
         model.addAttribute("project", new ProjectWriteModel());
-        model.addAttribute("message", "Project added successfully.");
+        model.addAttribute("message", "Project added successfully");
         return "projects";
     }
 
     @PostMapping(params = "addStep")
     String addProjectStep(@ModelAttribute("project") ProjectWriteModel currentProject) {
         currentProject.getSteps().add(new ProjectStep());
+        return "projects";
+    }
+
+    @PostMapping("/{id}")
+    String createGroup(@ModelAttribute("project") ProjectWriteModel currentProject,
+                       Model model,
+                       @PathVariable Long id,
+                       @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm") LocalDateTime deadline) {
+        try {
+            service.createGroup(deadline, id);
+            model.addAttribute("message", "Group created successfully");
+        } catch (IllegalStateException | IllegalArgumentException e) {
+            model.addAttribute("message", "Error while creating group");
+        }
         return "projects";
     }
 
