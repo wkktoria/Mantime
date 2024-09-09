1ac88d6845e4b135030f4999bf881fbb31c673c9
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2024-09-09 22:46:36 +0200

    Add asynchronism

--- src/main/java/io/github/wkktoria/mantime/reports/ChangedTaskEventListener.java
+++ src/main/java/io/github/wkktoria/mantime/reports/ChangedTaskEventListener.java
@@ -6,6 +6,7 @@ import io.github.wkktoria.mantime.model.event.TaskUndone;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,11 +19,13 @@ class ChangedTaskEventListener {
         this.repository = repository;
     }
 
+    @Async
     @EventListener
     public void on(TaskDone event) {
         onChanged(event);
     }
 
+    @Async
     @EventListener
     public void on(TaskUndone event) {
         onChanged(event);
